498724f008ac3c04ed94f156be420e4f505196d4
lamas14 <lamas14@mycu.concord.edu>
2013-08-09 15:03:42 -0400

    Added more Text Labels, Text fields and button

--- src/com/saugatlama/lamachat/Login.java
+++ src/com/saugatlama/lamachat/Login.java
@@ -2,26 +2,36 @@ package com.saugatlama.lamachat;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtUsername;
+	private JTextField txtAddress;
+	private JTextField txtPort;
 
 	/**
 	 * Create the frame.
 	 */
 	public Login() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 		
 		setResizable(false);
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(300, 350);
+		setSize(300, 320);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -29,13 +39,43 @@ public class Login extends JFrame {
 		contentPane.setLayout(null);
 		
 		txtUsername = new JTextField();
-		txtUsername.setBounds(118, 76, 135, 20);
+		txtUsername.setBounds(121, 51, 135, 20);
 		contentPane.add(txtUsername);
 		txtUsername.setColumns(10);
 		
 		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setBounds(39, 79, 69, 14);
+		lblUsername.setBounds(42, 54, 69, 14);
 		contentPane.add(lblUsername);
+		
+		txtAddress = new JTextField();
+		txtAddress.setBounds(121, 88, 135, 20);
+		contentPane.add(txtAddress);
+		txtAddress.setColumns(10);
+		
+		JLabel lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setBounds(42, 91, 69, 14);
+		contentPane.add(lblIpAddress);
+		
+		JLabel lblPort = new JLabel("Port:");
+		lblPort.setBounds(42, 150, 46, 14);
+		contentPane.add(lblPort);
+		
+		txtPort = new JTextField();
+		txtPort.setBounds(121, 147, 135, 20);
+		contentPane.add(txtPort);
+		txtPort.setColumns(10);
+		
+		JLabel lblAddressDesc = new JLabel("(e.g, 192.168.0.2)");
+		lblAddressDesc.setBounds(141, 111, 100, 14);
+		contentPane.add(lblAddressDesc);
+		
+		JLabel lblPortDesc = new JLabel("(e.g, 8192)");
+		lblPortDesc.setBounds(158, 171, 69, 14);
+		contentPane.add(lblPortDesc);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(102, 234, 89, 23);
+		contentPane.add(btnLogin);
 	}
 
 	/**
