1f28930d53f15fd45a7badb2e22801e20b0d4605
lamas14 <lamas14@mycu.concord.edu>
2013-08-10 16:50:47 -0400

    history text field

--- src/com/saugatlama/lamachat/Client.java
+++ src/com/saugatlama/lamachat/Client.java
@@ -6,6 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextArea;
+import java.awt.GridBagConstraints;
 
 public class Client extends JFrame {
 	
@@ -37,8 +40,22 @@ public class Client extends JFrame {
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{20, 855, 5}; // SUM should equal 880
+		gbl_contentPane.rowHeights = new int[]{40, 470, 40}; // SUM should equal 550
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JTextArea txtrHistory = new JTextArea();
+		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
+		gbc_txtrHistory.gridx = 1;
+		gbc_txtrHistory.gridy = 1;
+		contentPane.add(txtrHistory, gbc_txtrHistory);
+		
 		setVisible(true);
 	}
 
