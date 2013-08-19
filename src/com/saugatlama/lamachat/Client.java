5eb4c52e24b8d963301d9127299f9da792128cd9
lamas14 <lamas14@mycu.concord.edu>
2013-08-19 13:36:58 -0400

    Edited client layout

--- src/com/saugatlama/lamachat/Client.java
+++ src/com/saugatlama/lamachat/Client.java
@@ -1,14 +1,16 @@
 package com.saugatlama.lamachat;
 
-import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridBagLayout;
-import javax.swing.JTextArea;
-import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import javax.swing.JTextField;
 
 public class Client extends JFrame {
 	
@@ -17,6 +19,7 @@ public class Client extends JFrame {
 	
 	private String name, address;
 	private int port;
+	private JTextField txtMessage;
 
 	/**
 	 * Create the frame.
@@ -43,19 +46,39 @@ public class Client extends JFrame {
 		setContentPane(contentPane);
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{20, 855, 5}; // SUM should equal 880
+		gbl_contentPane.columnWidths = new int[]{23, 822, 30, 5}; // SUM should equal 880
 		gbl_contentPane.rowHeights = new int[]{40, 470, 40}; // SUM should equal 550
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
 		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JTextArea txtrHistory = new JTextArea();
+		txtrHistory.setEditable(false);
 		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.insets = new Insets(5, 5, 0, 0);
 		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
 		gbc_txtrHistory.gridx = 1;
 		gbc_txtrHistory.gridy = 1;
+		gbc_txtrHistory.gridwidth = 2;
 		contentPane.add(txtrHistory, gbc_txtrHistory);
 		
+		txtMessage = new JTextField();
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 1;
+		gbc_txtMessage.gridy = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		contentPane.add(btnSend, gbc_btnSend);
+		
+		txtMessage.requestFocus();
 		setVisible(true);
 	}
 
